71c4e8a96f44e8ce68094076aca8f59cc36ebdf1
Bishwajit jha <ajitjha393@gmail.com>
2020-05-14 14:20:26 +0530

    Added ejs templates

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,9 @@ import shopRoutes from './routes/shop';
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import expressHbs from 'express-handlebars';
 const app = express();
 
-app.engine(
-	'hbs',
-	expressHbs({
-		layoutsDir: 'views/layouts/',
-		defaultLayout: 'main-layout',
-		extname: 'hbs',
-	})
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(
@@ -37,7 +28,10 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
 	// res.status(404).sendFile(path.join(rootDir, '..', 'views', '404.html'));
 
-	res.status(404).render('404', { pageTitle: '404 Page Not Found' });
+	res.status(404).render('404', {
+		pageTitle: '404 Page Not Found',
+		path: null,
+	});
 });
 
 // Alternative
