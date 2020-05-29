ae812a545bd59665980698b68ff461e7793e5465
Bishwajit jha <ajitjha393@gmail.com>
2020-05-29 17:52:11 +0530

    Just added Extra func provided by mongoose

--- src/controllers/shop.ts
+++ src/controllers/shop.ts
@@ -4,6 +4,8 @@ import Product from '../models/product';
 
 export const getIndexPage: RequestHandler = async (_req, res, _next) => {
 	const products = await Product.find();
+	// Can also add diff filtering
+	// const products = await Product.find().select('title imageUrl').populate('userId);
 	console.log('Products Fetched....');
 	res.render('shop/index', {
 		products,
