3015f98742f59c0ac77c828a5ea64a21b5309104
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-11 10:59:35 +0530

    Added Controller for 500 code

--- src/controllers/error.ts
+++ src/controllers/error.ts
@@ -1,8 +1,18 @@
 import { RequestHandler } from 'express'
 
+// Page Not Found
 export const get404Page: RequestHandler = (req, res, _next) => {
 	res.status(404).render('404', {
 		pageTitle: '404 Page Not Found',
+		path: null,
+		isAuthenticated: req.isLoggedIn,
+	})
+}
+
+// Server Side error
+export const get500Page: RequestHandler = (req, res, _next) => {
+	res.status(500).render('500', {
+		pageTitle: 'Error!',
 		path: null,
 		isAuthenticated: req.isLoggedIn,
 	})
