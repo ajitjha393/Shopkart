e1631814eed276669086afe5ab8730dc196e9eb6
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-12 22:20:53 +0530

    Added Delete Image file logic after prod del

--- src/utils/fileUtility.ts
+++ src/utils/fileUtility.ts
@@ -1,15 +1,23 @@
-import fs from 'fs';
+import fs from 'fs'
 
 export function readFile<T>(path: string, cb: Function) {
 	return new Promise<T>((res, rej) => {
 		fs.readFile(path, (err, data) => {
-			res(cb(err, data));
-		});
-	});
+			res(cb(err, data))
+		})
+	})
 }
 
 export function writeFile<T>(p: string, products: T) {
-	fs.writeFile(p, JSON.stringify(products), err =>
+	fs.writeFile(p, JSON.stringify(products), (err) =>
 		err ? console.log(err) : null
-	);
+	)
+}
+
+export const deleteFile = (filePath: string) => {
+	fs.unlink(filePath, (err) => {
+		if (err) {
+			throw err
+		}
+	})
 }
